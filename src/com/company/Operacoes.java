4fd3e51cc979120d5ba7461f2034b533720872f9
ViiniciusAzevedo <vinicius.azevedo2422@gmail.com>
2020-09-21 18:58:04 -0300

    Calculadora Finalizada

---
+++ src/com/company/Operacoes.java
@@ -0,0 +1,24 @@
+package com.company;
+
+public class Operacoes {
+
+    public float soma(float numA, float numB) {
+        float resultado = numA + numB;
+        return resultado;
+    }
+
+    public float subtrai(float numA, float numB) {
+        float resultado = numA - numB;
+        return resultado;
+    }
+
+    public float multiplica(float numA, float numB) {
+        float resultado = numA * numB;
+        return resultado;
+    }
+
+    public float divide(float numA, float numB) {
+        float resultado = numA / numB;
+        return resultado;
+    }
+}
